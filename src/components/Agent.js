c9507f9f3979fb6c10b69f10805fcb37cc6c000c
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-06-12 23:52:01 -0400

    commit tree ca962f634aecc9fc6bdf8f567ee45a7ad97c9cbd

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Listings from "./Listings";
 
 const Agent = () => {
-    const [agents, setAgents] = useState([]);
+    const [agent, setAgents] = useState([]);
 
-    const params = useParams
+    const params = useParams()
 
     useEffect(() => {
-      fetch(`http//:localhost:9292/agents/${params.id}`)
+
+      fetch(`http://localhost:9292/agents/${params.id}`)
         .then((r) => r.json())
-        .then((items) => console.log(items));
+        .then(items => setAgents(items));
+
     }, []);
 
     return (
-    <div>
-
-    </div>
-  );
+      <div className="listing_list">
+        {agent.listings.map(l => <Listings key={l.id} listing={l}/>)}
+      </div>
+    );
 }
 
 export default Agent
